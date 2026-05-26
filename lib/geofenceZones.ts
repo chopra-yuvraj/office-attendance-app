a9d675c58d7c6a49242050c897032937222ec186
Yuvraj <yuvrajchopra19@gmail.com>
2026-05-27 01:23:42 +0530

    Update geofenceZones.ts

--- lib/geofenceZones.ts
+++ lib/geofenceZones.ts
@@ -9,7 +9,7 @@ import { GeofenceZone } from './geofence';
 
 export const OFFICE_ZONES: GeofenceZone[] = [
   { name: 'Office', lat: 21.169007723509296, lng: 72.8316816282585, radiusMeters: 150 },
-  { name: 'Home 902', lat: 21.14629032147555, lng: 72.78065647999507, radiusMeters: 100 },
+  { name: 'Home 902', lat: 21.146287701595472,lng: 72.78065499265989, radiusMeters: 100 },
 ];
 
 export const FACTORY_ZONES: GeofenceZone[] = [
